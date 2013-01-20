a24ce2a55dcc59d475ca2870fd4a3dd3e7839391
Adam Somers <adsomers@gmail.com>
2013-01-20 10:25:43 -0800

    Update to work with Leap SDK 0.7+

--- src/GLstuff.cpp
+++ src/GLstuff.cpp
@@ -43,8 +43,8 @@ void HarpListener::onFrame(const Leap::Controller& controller) {
     GLStuff::gLock.unlock();
     // Get the most recent frame and report some basic information
     const Leap::Frame frame = controller.frame();
-    const std::vector<Leap::Hand>& hands = frame.hands();
-    const size_t numHands = hands.size();
+    const Leap::HandList& hands = frame.hands();
+    const size_t numHands = hands.count();
     //    std::cout << "Frame id: " << frame.id()
     //    << ", timestamp: " << frame.timestamp()
     //    << ", hands: " << numHands << std::endl;
@@ -56,17 +56,16 @@ void HarpListener::onFrame(const Leap::Controller& controller) {
             
             
             // Check if the hand has any fingers
-            const std::vector<Leap::Finger>& fingers = hand.fingers();
-            const size_t numFingers = fingers.size();
+            const Leap::FingerList& fingers = hand.fingers();
+            const size_t numFingers = fingers.count();
             if (numFingers >= 1) {
                 // Calculate the hand's average finger tip position
                 Leap::Vector pos(0, 0, 0);
                 for (size_t i = 0; i < numFingers; ++i) {
                     const Leap::Finger& finger = fingers[i];
-                    const Leap::Ray& tip = finger.tip();
-                    pos.x += tip.position.x;
-                    pos.y += tip.position.y;
-                    pos.z += tip.position.z;
+                    pos.x += finger.tipPosition().x;
+                    pos.y += finger.tipPosition().y;
+                    pos.z += finger.tipPosition().z;
                     
                     std::map<int, GLStuff::Finger>::iterator iter = GLStuff::gFingers.find(finger.id());
                     if (iter == GLStuff::gFingers.end())
@@ -82,12 +81,12 @@ void HarpListener::onFrame(const Leap::Controller& controller) {
                     GLStuff::Finger* f = &(*iter).second;
                     float prevX = f->fX;
                     float prevY = f->fY;
-                    float newX = ((tip.position.x + 200) / 400.f);// * GLStuff::gWidth;
-                    float newY = ((tip.position.y - 150) / 200.f);// * GLStuff::gHeight;
+                    float newX = ((finger.tipPosition().x + 200) / 400.f);// * GLStuff::gWidth;
+                    float newY = ((finger.tipPosition().y - 150) / 200.f);// * GLStuff::gHeight;
                     f->SetX(newX);
                     f->SetY(newY);
-                    f->scaleFactor = 1 - (tip.position.z / 300.f);
-                    GLStuff::airMotion(newX, newY, tip.position.z, prevX , prevY);
+                    f->scaleFactor = 1 - (finger.tipPosition().z / 300.f);
+                    GLStuff::airMotion(newX, newY, finger.tipPosition().z, prevX , prevY);
                     
                 }
                 pos = Leap::Vector(pos.x/numFingers, pos.y/numFingers, pos.z/numFingers);
@@ -98,38 +97,6 @@ void HarpListener::onFrame(const Leap::Controller& controller) {
                 
                 //            std::cout << "Hand has " << numFingers << " fingers with average tip position"
                 //            << " (" << pos.x << ", " << pos.y << ", " << pos.z << ")" << std::endl;
-            }
-            
-            // Check if the hand has a palm
-            const Leap::Ray* palmRay = hand.palm();
-            if (palmRay != NULL) {
-                // Get the palm position and wrist direction
-                const Leap::Vector palm = palmRay->position;
-                const Leap::Vector wrist = palmRay->direction;
-                //            std::cout << "Palm position: ("
-                //            << palm.x << ", " << palm.y << ", " << palm.z << ")" << std::endl;
-                
-                // Check if the hand has a normal vector
-                const Leap::Vector* normal = hand.normal();
-                if (normal != NULL) {
-                    // Calculate the hand's pitch, roll, and yaw angles
-                    double pitchAngle = -atan2(normal->z, normal->y) * 180/M_PI + 180;
-                    double rollAngle = -atan2(normal->x, normal->y) * 180/M_PI + 180;
-                    double yawAngle = atan2(wrist.z, wrist.x) * 180/M_PI - 90;
-                    // Ensure the angles are between -180 and +180 degrees
-                    if (pitchAngle > 180) pitchAngle -= 360;
-                    if (rollAngle > 180) rollAngle -= 360;
-                    if (yawAngle > 180) yawAngle -= 360;
-                    //                std::cout << "Pitch: " << pitchAngle << " degrees,  "
-                    //                << "roll: " << rollAngle << " degrees,  "
-                    //                << "yaw: " << yawAngle << " degrees" << std::endl;
-                }
-            }
-            
-            // Check if the hand has a ball
-            const Leap::Ball* ball = hand.ball();
-            if (ball != NULL) {
-                //            std::cout << "Hand curvature radius: " << ball->radius << " mm" << std::endl;
             }
         }
     }
@@ -186,7 +153,8 @@ namespace GLStuff
     
     void GLStartup(int argc, const char** argv)
     {
-        Leap::Controller controller(&listener);
+        Leap::Controller controller;
+        controller.addListener(listener);
         
         std::cout << "Hit q to quit.\n";
         
@@ -576,7 +544,7 @@ namespace GLStuff
             glColor4f(1,1,1,1);
             std::string s = "";      
             glLineWidth(2);
-            s = "AirHarp 0.0.1";
+            s = "AirHarp 0.1.1";
             DrawString(gWidth - 500, gHeight-20, s, GLUT_BITMAP_TIMES_ROMAN_10, 0.15);
         }
         glFlush( );
